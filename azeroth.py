651baf592cc1dcaa18cbf0645c95e311073e8862
jnxldr <47059809+jnxldr@users.noreply.github.com>
2019-08-25 09:39:41 +0200

    Fix inconsistent upload dates from github Rename update_date to upload_date
    Signed-off-by: jnxldr <jnxldr+github@gmail.com>

--- azeroth.py
+++ azeroth.py
@@ -19,17 +19,18 @@ def get_curse(url):
         addon_table = target_game_version.find_parent("tr")
 
         # Addon Version
-        addon_version = addon_table.a.text
+        # Omitted until storing version in Sheets, so we can do comparisons
+        # addon_version = addon_table.a.text
 
-        # Last Updated Date
+        # Last Upload Date
         update_epoch = addon_table.abbr['data-epoch']
-        update_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
+        upload_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
 
         # Download Path
         href_path = 'https://www.curseforge.com' + addon_table.a['href']
         download_path = href_path.replace('files', 'download')
 
-        return addon_name, addon_version, update_date, download_path
+        return addon_name, upload_date, download_path
 
     except (AttributeError, IndexError):
         pass
@@ -40,76 +41,21 @@ def get_curse(url):
         addon_table = target_game_version.find_parent("tr")
 
         # Addon Version
-        addon_version = addon_table.a.text
+        # Omitted until storing version in Sheets, so we can do comparisons
+        # addon_version = addon_table.a.text
 
-        # Last Updated Date
+        # Last Upload Date
         update_epoch = addon_table.abbr['data-epoch']
-        update_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
+        upload_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
 
         # Download Path
         href_path = 'https://www.curseforge.com' + addon_table.a['href']
         download_path = href_path.replace('files', 'download')
 
-        return addon_name, addon_version, update_date, download_path
+        return addon_name, upload_date, download_path
 
     except (AttributeError, IndexError):
         pass
-
-
-'''
-    # This block isn't currently required.
-    # These were workarounds that were useful in the situations described.
-    # Now that the variable 'classic_version_files' is used, we always get all the 1.13.2 addon only
-    
-    try:
-        # If the first instance of a 1.13.2 file is also the latest version of the addon
-        # but isn't exclusively for Classic
-        # I.e., the 'Main File' on the page of a Retail addon ported to Classic
-
-        # Addon Info Table
-        target_game_version = soup.find_all(string=re.compile('1.13.2'))[1]
-        addon_table = target_game_version.find_parent("article")
-
-        # Addon Version
-        addon_version = addon_table.h3.text
-
-        # # Download Path
-        # file_path = 'https://www.curseforge.com' + addon_table.a['href']
-        # download_path = file_path.replace('files', 'download')
-
-        # Last Updated Date
-        update_epoch = addon_table.abbr['data-epoch']
-        update_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
-
-        return addon_name, addon_version, update_date
-
-    except (AttributeError, IndexError):
-        pass
-
-    try:
-        # If the first instance of a 1.13.2 file is also the latest version of a Classic exclusive addon
-        # I.e., the 'Main File' on the page
-
-        # Addon Info Table
-        target_game_version = soup.find_all(string=re.compile('1.13.2'))[1]
-        addon_table = target_game_version.find_parent("article")
-
-        # Addon Version
-        addon_version = addon_table.h3.text
-
-        # # Download Path
-        # file_path = 'https://www.curseforge.com' + addon_table.a['href']
-        # download_path = file_path.replace('files', 'download')
-
-        # Last Updated Date
-        update_epoch = addon_table.abbr['data-epoch']
-        update_date = datetime.fromtimestamp(int(update_epoch)).strftime('%Y-%m-%d')
-
-        return addon_name, addon_version, update_date
-
-    except (AttributeError, IndexError):
-        pass
-'''
 
 
 def get_wowi(url):
@@ -120,17 +66,18 @@ def get_wowi(url):
     addon_name = soup.find('meta', property='og:title')['content']
 
     # Addon Version
-    addon_version = soup.find('div', id='version').text.replace('Version: ', '')
+    # Omitted until storing version in Sheets, so we can do comparisons
+    # addon_version = soup.find('div', id='version').text.replace('Version: ', '')
 
-    # Last Updated Date
+    # Last Upload Date
     date_modified = str(soup.find('div', id='safe').text.replace('Updated: ', '')[:-9])
-    update_date = datetime.strptime(date_modified, '%m-%d-%y').date().strftime('%Y-%m-%d')
+    upload_date = datetime.strptime(date_modified, '%m-%d-%y').date().strftime('%Y-%m-%d')
 
     # Download Path
     replace_path = url.replace('info', 'download')
     download_path = replace_path.replace('.html', '')
 
-    return addon_name, addon_version, update_date, download_path
+    return addon_name, upload_date, download_path
 
 
 def get_github(url):
@@ -141,17 +88,18 @@ def get_github(url):
     raw_addon_name = soup.find('meta', property='og:title')['content']
     addon_name = raw_addon_name.rsplit('/', 1)[1]
 
-    # Addon Version
-    # addon_version = soup.find('div', id='version').text.replace('Version: ', '')
+    # Addon Info Table
+    target_version = soup.find(string=re.compile('Latest commit'))
+    addon_table = target_version.find_parent("div")
 
-    # Last Updated Date
-    date_modified = soup.find('span', itemprop='dateModified').text.replace(',', '')
-    update_date = datetime.strptime(date_modified, '%b %d %Y').date().strftime('%Y-%m-%d')
+    # Last Upload Date
+    age_data = addon_table.find({'relative-time': 'datetime'})
+    raw_time = age_data['datetime']
+    upload_date = raw_time[:10]
 
     # Download Path
     get_path = soup.find(string=re.compile('Download ZIP'))
     path_table = get_path.find_parent("a")
     download_path = 'https://github.com' + path_table.get('href')
 
-    return addon_name, update_date, download_path
-
+    return addon_name, upload_date, download_path
--- classy.py
+++ classy.py
@@ -19,58 +19,43 @@ def main():
 
         for stored_addon_info in addon_list:
             url = stored_addon_info[2]
-            last_update = stored_addon_info[4]
+            last_upload = stored_addon_info[4]
             domain_extract = tldextract.extract(url).domain
 
             if domain_extract in 'curseforge':
                 live_addon_info = get_curse(url)
-                if live_addon_info[2] != last_update:
+                if live_addon_info[1] != last_upload:
                     print('Name: ', live_addon_info[0])
-                    print('Current Version: ', live_addon_info[1])
-                    print('Current Upload Date: ', live_addon_info[2])
-                    print('Stored Upload Date: ', last_update)
-                    print('Download: ', live_addon_info[3], '\n')
+                    # print('Current Version: ', live_addon_info[1]) # Omitted until storing version in Sheets
+                    print('Current Upload Date: ', live_addon_info[1])
+                    print('Stored Upload Date: ', last_upload)
+                    print('Addon Page: ', url)
+                    print('Direct Download: ', live_addon_info[2], '\n')
 
             elif domain_extract in 'wowinterface':
                 live_addon_info = get_wowi(url)
-                if live_addon_info[2] != last_update:
+                if live_addon_info[1] != last_upload:
                     print('Name: ', live_addon_info[0])
-                    print('Current Version: ', live_addon_info[1])
-                    print('Current Upload Date: ', live_addon_info[2])
-                    print('Stored Upload Date: ', last_update)
-                    print('Download: ', live_addon_info[3], '\n')
+                    # print('Current Version: ', live_addon_info[1]) # Omitted until storing version in Sheets
+                    print('Current Upload Date: ', live_addon_info[1])
+                    print('Stored Upload Date: ', last_upload)
+                    print('Addon Page: ', url)
+                    print('Direct Download: ', live_addon_info[2], '\n')
 
             elif domain_extract in 'github':
                 live_addon_info = get_github(url)
-                if live_addon_info[1] != last_update:
+                if live_addon_info[1] != last_upload:
                     print('Name: ', live_addon_info[0])  # Different index  for github as I don't return the version
                     print('Current Upload Date: ', live_addon_info[1])
-                    print('Stored Upload Date: ', last_update)
-                    print('Download: ', live_addon_info[2], '\n')
+                    print('Stored Upload Date: ', last_upload)
+                    print('Addon Page: ', url)
+                    print('Direct Download: ', live_addon_info[2], '\n')
                 pass
             else:
                 pass
 
     print('Done checking the spreadsheet.')
 
+
 if __name__ == '__main__':
     main()
-
-    '''
-    url = 'https://www.curseforge.com/wow/addons/buffwatch-classic/files'
-    print(get_curse(url))
-
-    url = 'https://www.wowinterface.com/downloads/info24919-VuhDoClassic.html'
-    print(get_wowi(url))
-
-    github_addon_list = ('https://github.com/AeroScripts/QuestieDev',
-                         'https://github.com/cannonpalms/FasterLooting',
-                         'https://github.com/kesava-wow/kuinameplates2/tree/classic',
-                         'https://github.com/shirsig/Cleanup',
-                         'https://github.com/shirsig/SortBags/tree/retail',
-                         'https://github.com/smp4903/FiveSecondRule/')
-
-    url = 'https://github.com/cannonpalms/FasterLooting'
-    for url in github_addon_list:
-        print(get_github(url), '\n')
-    '''
